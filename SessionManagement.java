01fccf088c716edeafcbec1a1b058a9d52a1ada3
bypixelpro <info@pixelpro.es>
2017-10-17 15:55:42 +0200

    02 Manejador de la sesión

---
+++ SessionManagement.java
@@ -0,0 +1,42 @@
+package es.pixelpro.david.preferencias;
+
+/**
+ * Created by pixelpro on 17/10/17.
+ */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+public class SessionManagement {
+    // Shared Preferences
+    SharedPreferences pref;
+
+    // Editor for Shared preferences
+    Editor editor;
+
+    // Context
+    Context _context;
+
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Sharedpref file name
+    private static final String PREF_NAME = "Pixelpro";
+
+    // All Shared Preferences Keys
+    private static final String IS_LOGIN = "IsLoggedIn";
+
+    // User name (make variable public to access from outside)
+    public static final String KEY_NAME = "name";
+
+    // Email address (make variable public to access from outside)
+    public static final String KEY_EMAIL = "email";
+
+    // Constructor
+    public SessionManagement(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+}
